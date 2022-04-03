babc730fd6f76aa05b212fe60cec82938118d785
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-04-03 08:36:56 +0530

    handled no search results case

--- src/Pages/search-page/search-page.js
+++ src/Pages/search-page/search-page.js
@@ -1,4 +1,5 @@
 import { useParams } from 'react-router-dom'
+import noResults from '../../images/noResults.svg'
 import { useEffect } from 'react'
 import { Card, Loader } from '../../components/composite'
 import { useData } from '../../helpers/contexts/data-context'
@@ -8,6 +9,14 @@ export function SearchPage() {
     const { keyword } = useParams()
     const { finalArray } = useFilter()
     const { dispatchData, popups, setPopups } = useData()
+    const result = finalArray.filter(
+        (resultItem) =>
+            (resultItem.name.toLowerCase().includes(keyword.toLowerCase()) ||
+                resultItem.brand
+                    .toLowerCase()
+                    .includes(keyword.toLowerCase())) &&
+            resultItem.inStock
+    )
     const { loader } = popups
     useEffect(() => {
         getProducts(setPopups, dispatchData)
@@ -20,9 +29,21 @@ export function SearchPage() {
             <Loader />
             <h2>Showing you the search results! Hang on</h2>
         </div>
+    ) : result.length === 0 ? (
+        <div className="flex flex-column align-center">
+            <img
+                src={noResults}
+                alt="No results found"
+                style={{ width: '100vw', height: '80vh' }}
+            />
+            <h1>No results found, put in some other keyword</h1>
+        </div>
     ) : (
         <div className="flex align-center flex-column align-centerflex-wrap">
-            <marquee>Filtered using brand name and product name</marquee>
+            <marquee>
+                Filtered using brand name and product name and showing items
+                which are in stock
+            </marquee>
             <h1 className="size-16">Search Page </h1>
             <p>
                 Showing search results for items matching the word &apos;
